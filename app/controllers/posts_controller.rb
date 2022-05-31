db8b0738c2d9229c420f39485b4da326221175ac
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-05-31 15:10:01 +0900

    記事投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new 
+    @post = Post.new
+  end
+
+  def create 
+    @post = Post.new(post_params)
+    puts @post
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body, :user_id)
+    end
 end
