64f66bb830a348e488467023b40c9351bba389d8
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-05-31 14:11:35 +0900

    記事一覧表示機能と記事詳細表示機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   root "posts#index"
 
-  resource :posts
+  # get "/", to: "posts#index"
+  # get "/posts/:id", to: "posts#show"
+
+  resources :posts
   
   # get "/", to: "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
