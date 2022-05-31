c0da57800bd5a92054f0636c3c08fddd2ec9d079
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-05-31 12:14:16 +0900

    postsテーブルを作成し、userモデルとpostモデルのリレーションを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root "homes#index"
+  root "posts#index"
 
-  get "/", to: "homes#index"
+  resource :posts
+  
+  # get "/", to: "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
